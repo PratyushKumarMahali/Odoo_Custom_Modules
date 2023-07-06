a24c5b598ff4e2de0cf3fdae8b8ec38796c0ee57
Pratyush Kumar Mahali <83828190+PratyushKumarMahali@users.noreply.github.com>
2023-07-06 12:06:06 +0530

    Delete online_payment_system directory

--- online_payment_system/quocent_customer_portal_signup/controllers/quocent_exception.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Quocent Pvt. Ltd.
-# Copyright (C) Quocent Pvt. Ltd.
-# All Rights Reserved
-# https://www.quocent.com
-
-class DetailsNotFoundError(Exception):
-    pass
--- online_payment_system/quocent_customer_portal_signup/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Quocent Pvt. Ltd.
-# Copyright (C) Quocent Pvt. Ltd.
-# All Rights Reserved
-# https://www.quocent.com
-
-from . import qcent_res_partner_inhe, qcent_res_config_inhe
